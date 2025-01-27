7e95c2dc8fcd05a01d20054c2ac481eefb4beabe
avish madushanka <avishmadushanka21@gmail.com>
2025-01-28 03:07:35 +0530

    update slider

--- src/Components/HomePageComponents/Homemain.jsx
+++ src/Components/HomePageComponents/Homemain.jsx
@@ -7,13 +7,13 @@ function App() {
     <div className="App">
       <Slider>
         <div className='slide1'>
-          <img src="https://www.bramptonwindows.co.uk/wp-content/uploads/2023/08/Aluminium-Slider-Doors-Black.jpg" alt="Cat 1" />
+        <img src="https://www.bradnams.com.au/wp-content/uploads/2020/02/Two-White-Double-Hinged-Doors.jpg" alt="Cat 1" />
         </div>
         <div className='slide2'>
             <img src="https://www.swisstekaluminium.com/wp-content/uploads/2022/11/Door-Banner.jpg" alt="Cat 2" />
         </div>
         <div className='slide3'>
-             <img src="https://www.bradnams.com.au/wp-content/uploads/2020/02/Two-White-Double-Hinged-Doors.jpg" alt="Cat 3" />
+            <img src="https://www.bramptonwindows.co.uk/wp-content/uploads/2023/08/Aluminium-Slider-Doors-Black.jpg" alt="Cat 3" /> 
         </div>
       </Slider>
     </div>
