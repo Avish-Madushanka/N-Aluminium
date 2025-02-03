9f308484487558627359eb7a707cd9119b612806
avish <avishmadushanka21@gmail.com>
2025-02-03 14:45:37 +0530

    signup

---
+++ src/Components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../RegistrationForm/RegistrationForm.css'; // Import CSS file for styling
+
+const HomePage = () => {
+  return (
+    <div className="background-container">
+      <div className="button-row">
+        <button className="button">Button 1</button>
+        <button className="button">Button 2</button>
+        <button className="button">Button 3</button>
+        <button className="button">Button 4</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
