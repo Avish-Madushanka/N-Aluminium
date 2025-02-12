4726e01a7adbe100c517b7d5814a4c7aac78d543
avish <avishmadushanka21@gmail.com>
2025-02-12 14:14:20 +0530

    update reg page

--- src/Components/RegistrationForm/RegistrationForm.jsx
+++ src/Components/RegistrationForm/RegistrationForm.jsx
@@ -5,9 +5,9 @@ const RegistrationForm = () => {
   return (
     <div className="background-container">
       <div className="button-row">
-        <button className="button">Button 1</button>
-        <button className="button">Button 2</button>
-        <button className="button">Button 3</button>
+        <button className="button">Admin</button>
+        <button className="button">Business Owners</button>
+        <button className="button">Clients</button>
         <button className="button1">Sign Up</button>
       </div>
     </div>
