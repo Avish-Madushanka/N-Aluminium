6a076251940a2734c7ecb1b3e55caa4f377892b1
avish <avishmadushanka21@gmail.com>
2025-02-14 20:00:38 +0530

    update about us

--- src/Components/AboutUs/AboutUSDes.jsx
+++ src/Components/AboutUs/AboutUSDes.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import '../AboutUs/AboutUsDes.css'; 
+import './AboutUSDes.css';
 
-function AboutUsDes() {
+function AboutUSDes() {
+  const mainImageUrl = "https://wallpapers.com/images/hd/dynamic-battlefield-1-action-in-4k-resolution-ookn4al8l9472frf.jpg";
+  const secondImageUrl = "https://c4.wallpaperflare.com/wallpaper/668/51/763/4k-zavod-graveyard-shift-night-operations-battlefield-4-wallpaper-preview.jpg";
+  const thirdImageUrl = "https://c4.wallpaperflare.com/wallpaper/668/51/763/4k-zavod-graveyard-shift-night-operations-battlefield-4-wallpaper-preview.jpg";
+  const fourthImageUrl = "https://c4.wallpaperflare.com/wallpaper/668/51/763/4k-zavod-graveyard-shift-night-operations-battlefield-4-wallpaper-preview.jpg";
+  const descriptionText = "what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? what we do? v";
+
   return (
-    <div>
-      <main>
-        <div>
-          <section>
-            <h2>Our Environment, Our Future</h2>
-            <h3>Help Us Reduce Environmental Pollution</h3>
-            <p>GWCN plays a vital role in the growing worldwide movement to reduce
-              the impact of Environmental pollution on society via advocacy and the
-              implementation of evidence-based programs to reduce pollution in
-              Oceans, Coastlines, Lands and the Atmosphere.</p>
-            <p>The purpose of the Global Waste Cleaning Network is to support
-              environmental activities, showcase and connect the network
-              members, contribute to environmental debates, foster environmental
-              research, and spread environment related information.</p>
-          </section>
-        </div>
-      </main>
-
-      <div className="translate-button">
-        <button>
-          Translate »
-        </button>
+    <div className="AboutDes-container">
+      <h1 className="main-title">what we do?</h1>
+      <img src={mainImageUrl} alt="Main" className="main-image" />
+      <div className="section">
+        <h2 className="section-title">what we do?</h2>
+        <p className="description">{descriptionText}</p>
       </div>
+
+      <div className="grid-container">
+        <div className="grid-item">
+          <img src={secondImageUrl} alt="Building" className="grid-image" />
+          <button className="image-button">Button</button>
+        </div>
+        <div className="grid-text">
+          <h2 className="section-title">what we do?</h2>
+          <p className="description">{descriptionText}</p>
+        </div>
+      </div>
+
+      <div className="grid-container">
+        <div className="grid-text">
+          <h2 className="section-title">what we do?</h2>
+          <p className="description">{descriptionText}</p>
+        </div>
+        <div className="grid-item">
+          <img src={fourthImageUrl} alt="Elephant" className="grid-image" />
+          <button className="image-button">Button</button>
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
 
-export default AboutUsDes;
+export default AboutUSDes;
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import AboutUsHeader from '../Components/AboutUs/AboutUsHeader';
 import AboutUsDes from '../Components/AboutUs/AboutUSDes';
 
 
 const AboutUs = () => {
   return (
     <div>
-      <AboutUsHeader />
       <AboutUsDes />
     </div>
   );
