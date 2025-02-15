608e13aaa12639a834bf7ed6ab5b5f7f67b3b281
avish <avishmadushanka21@gmail.com>
2025-02-15 14:59:27 +0530

    update buy and sell page

--- src/Components/BuyandSell/BSHeader.jsx
+++ src/Components/BuyandSell/BSHeader.jsx
@@ -39,6 +39,28 @@ const BSHeader = () => {
           </div>
         </div>
 
+        <div className="product-card">
+          <img src="https://i.redd.it/rturazpmucha1.jpg" alt="Product" className="product-image" />
+          <div className="product-details">
+            <h3>Aluminum Door</h3>
+            <p>An aluminum door is strong, lightweight, weather-resistant, durable, low-maintenance, and modern-looking.</p>
+            <p className="price">Rs: 10,000.00</p>
+            <p className="location">Kaluthara Western</p>
+            <button className="buy-button">Buy</button>
+          </div>
+        </div>
+
+        <div className="product-card">
+          <img src="https://i.redd.it/rturazpmucha1.jpg" alt="Product" className="product-image" />
+          <div className="product-details">
+            <h3>Aluminum Door</h3>
+            <p>An aluminum door is strong, lightweight, weather-resistant, durable, low-maintenance, and modern-looking.</p>
+            <p className="price">Rs: 10,000.00</p>
+            <p className="location">Kaluthara Western</p>
+            <button className="buy-button">Buy</button>
+          </div>
+        </div>
+
       </div>
     </div>
   );
