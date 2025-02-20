431521e05cd9b3233d0ed1f92dd2e9703d81e743
avish <avishmadushanka21@gmail.com>
2025-02-20 13:41:48 +0530

    update BOwnerHome

--- src/Components/BusinessOwner/BOwnerHeader.jsx
+++ src/Components/BusinessOwner/BOwnerHeader.jsx
@@ -5,7 +5,7 @@ const BOwnerHeader = () => {
   return (
     <div className="business1-card">
       <div className="cover1-photo">
-        <img src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png" alt="Cover1" className="cover1-image" />
+        <img src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjZWJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D" alt="Cover1" className="cover1-image" />
         <div className="profile1-photo">
           <img src="https://i.pinimg.com/736x/71/b3/e4/71b3e4159892bb319292ab3b76900930.jpg" alt="Profile1" className="profile1-image" />
         </div>
