de2800c2678b15eea731663336bb8928112c8e5e
avish <avishmadushanka21@gmail.com>
2025-02-22 11:45:06 +0530

    update collection header

---
+++ src/Components/WasteCollect/WasteDes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './WasteDes.css';
+
+const WasteDes = () => {
+  return (
+    <div className="WApickup-schedule-container">
+      <h2 className="WApage-title">Current Customer Garbage Pickup Schedule</h2>
+      <h1 className="WAschedule-title">Your Pickup Schedule</h1>
+      <p className="WAschedule-description">
+        You can only see your pickup schedule if you are a current customer.
+      </p>
+    </div>
+  );
+};
+
+export default WasteDes;
--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import WasteHeader from '../Components/WasteCollect/WasteHeader';
+import WasteDes from '../Components/WasteCollect/WasteDes';
 
 const Collection = () => {
   return (
     <div>
       <WasteHeader />
+      <WasteDes />
     </div>
   );
 };
