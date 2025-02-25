8f3f2c415da1a277356baad0bfe11a64e5290e9d
avish <avishmadushanka21@gmail.com>
2025-02-25 19:47:35 +0530

    update About us des 2

--- src/Components/BuyandSell/BSHeader.jsx
+++ src/Components/BuyandSell/BSHeader.jsx
@@ -45,7 +45,7 @@ const BSHeader = () => {
           <div className="product-card">
             <img src="https://i.redd.it/rturazpmucha1.jpg" alt="Product" className="product-image" />
             <div className="product-details">
-              <h3>Aluminum Door</h3>
+              <h3>Aluminum </h3>
               <p>An aluminum door is strong, lightweight, weather-resistant, durable, low-maintenance, and modern-looking.</p>
               <p className="price">Rs: 10,000.00</p>
               <p className="location">Kaluthara Western</p>
