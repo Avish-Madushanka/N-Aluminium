1f35f6f9b16fb7f499650e1bce2a8ead38523d25
avish <avishmadushanka21@gmail.com>
2025-02-26 01:03:02 +0530

    update footer css

--- src/Components/AboutUs/AboutUSDes3.jsx
+++ src/Components/AboutUs/AboutUSDes3.jsx
@@ -4,7 +4,7 @@ import './AboutUSDes3.css';
 const AboutUSDes3 = () => {
   const imageUrl1 = 'https://media.istockphoto.com/id/133993685/photo/scrap-metal-pieces-laying-in-a-pile.jpg?s=612x612&w=0&k=20&c=255eq6JeJao2iO5w-pXhmFkBGDHLabXH5Kn9SaA1lxw=';
   const imageUrl2 = 'https://s.alicdn.com/@sc04/kf/H72424df1f0d749e3bf5ce42a8e3ebac5y.png';
-  const imageUrl3 = 'https://i.imgur.com/4Nn9C9f.jpg';
+  const imageUrl3 = 'https://3.imimg.com/data3/VW/XS/MY-9845636/aluminium-composite-panel-scrap-500x500.jpg';
 
   return (
     <div className="xrecycling-container">
@@ -23,7 +23,7 @@ const AboutUSDes3 = () => {
         </div>
         <div className="ximage-item">
           <img src={imageUrl3} alt="Aluminum" className="xaluminum-image" />
-          <p className="xaluminum-text">Aluminum</p>
+          <p className="xaluminum-text">Cladding Boards Scraps </p>
         </div>
       </div>
     </div>
