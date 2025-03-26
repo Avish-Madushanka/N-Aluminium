2b8230bd4004ea47f459c79f511cf5da0bd5fe2a
avish <avishmadushanka21@gmail.com>
2025-03-26 19:52:46 +0530

    update collection page

--- src/Components/WasteCollect/WasteMain.jsx
+++ src/Components/WasteCollect/WasteMain.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import './WasteMain.css';
+import React from "react";
+import "./WasteMain.css";
 
-function WasteMain() {
-    const imageUrlSell = "https://salvage.jksalvageco.com/wp-content/uploads/2021/06/image-1.jpg";
-    const imageUrlBuy = "https://t4.ftcdn.net/jpg/09/48/95/49/360_F_948954917_QggcaJkTcmvaxNlK5nU2cZjfS7NRxBlD.jpg"; 
+const WasteMain = () => {
+  const imageUrlSell =
+    "https://salvage.jksalvageco.com/wp-content/uploads/2021/06/image-1.jpg";
+  const imageUrlBuy =
+    "https://t4.ftcdn.net/jpg/09/48/95/49/360_F_948954917_QggcaJkTcmvaxNlK5nU2cZjfS7NRxBlD.jpg";
 
-    return (
-        <div className="Wrecycling-section">
-            <div className="Wheader-content">
-                <div className="Wmain-heading">Industry Leader in Scrap Metal Recycling & Raw Material Conversion</div>
-                <div className="Wdescription">Metal Source strives to have the very best processes, technology, and operations to convert raw materials into renewable and reusable resources for the sustainable future and to the continual improvement of social, economic, and environmental well-being of the global community.</div>
+  return (
+    <div className="Wrecycling-section">
+        <div className="Wheader-content">
+            <div className="Wmain-heading">Industry Leader in Scrap Metal Recycling & Raw Material Conversion</div>
+            <div className="Wdescription">Metal Source strives to have the very best processes, technology, and operations to convert raw materials into renewable and reusable resources for the sustainable future and to the continual improvement of social, economic, and environmental well-being of the global community.</div>
+        </div>
+
+        <div className="waste-section">
+        <div className="waste-card-container">
+            <div className="waste-card">
+            <img src={imageUrlSell} alt="Sell to Us" className="waste-card-image" />
+            <div className="waste-card-content">
+                <h3 className="waste-card-title">Sell to Us</h3>
+                <p className="waste-card-description">
+                Metal Source is a buyer of all scrap metal grades including
+                aluminum, steel, copper, stainless, and all ferrous and nonferrous
+                metals.
+                </p>
+                <a href="/WastePickForm" className="waste-learn-more">→</a>
+            </div>
             </div>
 
-            <div className="Wcard-container">
-                <div className="Wcard">
-                    <img src={imageUrlSell} alt="Sell to Us" className="Wcard-image" />
-                    <div className="Wcard-content">
-                        <h3 className="Wcard-title">Sell to Us</h3>
-                        <p className="Wcard-description">Metal Source is a buyer of all scrap metal grades including aluminum, steel, copper, stainless, and all ferrous and nonferrous metals.</p>
-                        <a href="/WastePickForm" className="Wlearn-more">→</a>
-                    </div>
-                </div>
-
-                <div className="Wcard">
-                    <img src={imageUrlBuy} alt="Buy From Us" className="Wcard-image" />
-                    <div className="Wcard-content">
-                        <h3 className="Wcard-title">Buy From Us</h3>
-                        <p className="Wcard-description">Contact our Commercial Team for your ferrous and nonferrous recycled metal requirements, and we can assist you in obtaining the metal you need.</p>
-                        <a href="#" className="Wlearn-more">→</a>
-                    </div>
-                </div>
+            <div className="waste-card">
+            <img src={imageUrlBuy} alt="Buy From Us" className="waste-card-image" />
+            <div className="waste-card-content">
+                <h3 className="waste-card-title">Buy From Us</h3>
+                <p className="waste-card-description">
+                Contact our Commercial Team for your ferrous and nonferrous
+                recycled metal requirements, and we can assist you in obtaining
+                the metal you need.
+                </p>
+                <a href="#" className="waste-learn-more">→</a>
+            </div>
             </div>
         </div>
-    );
-}
+        </div>
+    </div>
+  );
+};
 
 export default WasteMain;
