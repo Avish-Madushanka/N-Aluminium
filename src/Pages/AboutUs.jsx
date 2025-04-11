bba5dfcad9357c712f7b99cf22ff91a58f7da53a
avish <avishmadushanka21@gmail.com>
2025-04-11 12:49:24 +0530

    css update

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import AboutUsHeader from '../Components/AboutUs/AboutUsHeader';
 import AboutUSDes from '../Components/AboutUs/AboutUSDes';
+import AboutUSDes2 from '../Components/AboutUs/AboutUSDes2';
+import AboutUSDes3 from '../Components/AboutUs/AboutUSDes3';
 
 const AboutUs = () => {
   return (
     <div>
       <AboutUsHeader />
       <AboutUSDes />
+      <AboutUSDes2 />
+      <AboutUSDes3 />
     </div>
   );
 };
