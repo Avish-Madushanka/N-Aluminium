724ef4d61c9d534cdf7b592f3bb33f65c6cabe5d
avish madushanka <avishmadushanka21@gmail.com>
2025-09-16 02:20:26 +0530

    home page update

--- Frontend/src/Components/MainPage/MainHomePage.jsx
+++ Frontend/src/Components/MainPage/MainHomePage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './MainHomePage.css';
+import { FaPlay } from 'react-icons/fa';
 
 const MainHomePage = () => {
   return (
     <div className="MHP-container">
       <div className="MHP-hero">
+        <div className="MHP-overlay"></div>
         <div className="MHP-heroContent">
-          <h1>WELCOME</h1>
+          <h1 className="MHP-title">WELCOME</h1>
+          <p className="MHP-description">
+            To MetaTrade
+          </p>
+          <div className="MHP-playButtonWrapper">
+          </div>
         </div>
       </div>
-      
+
       <div className="MHP-releases">
         <div className="MHP-postersRow">
           <div className="MHP-posterItem">
@@ -18,24 +25,28 @@ const MainHomePage = () => {
             </div>
             <button className="MHP-watchBtn">Watch Now</button>
           </div>
+
           <div className="MHP-posterItem">
             <div className="MHP-posterBox MHP-dogdaysPoster">
               <span>DOG DAYS</span>
             </div>
             <button className="MHP-watchBtn">Watch Now</button>
           </div>
+
           <div className="MHP-posterItem">
             <div className="MHP-posterBox MHP-grandfatherPoster">
               <span>How I Met Your Grand Father</span>
             </div>
             <button className="MHP-watchBtn">Watch Now</button>
           </div>
+
           <div className="MHP-posterItem">
             <div className="MHP-posterBox MHP-orangePoster">
               <span>Black Is The New Orange</span>
             </div>
             <button className="MHP-watchBtn">Watch Now</button>
           </div>
+
           <div className="MHP-posterItem">
             <div className="MHP-posterBox MHP-zooPoster">
               <span>Zoo Park</span>
