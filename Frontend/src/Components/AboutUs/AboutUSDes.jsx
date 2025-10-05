919dc5930ad3808959c4af7817b29bba2b2ed626
avish madushanka <avishmadushanka21@gmail.com>
2025-10-06 03:20:38 +0530

    update about us ui

--- Frontend/src/Components/AboutUs/AboutUSDes.jsx
+++ Frontend/src/Components/AboutUs/AboutUSDes.jsx
@@ -27,24 +27,24 @@ const AboutUsDes = () => {
   ];
 
   return (
-    <div className="AB-main-container">
+     <div className="AB-main-container">
       <div className="ABrecycling-container">
         <div className="ABimage-section">
           <img
             src="https://fairsalvage.com/wp-content/uploads/2022/05/A-Guide-To-Metal-Recycling-Process_image1.webp"
-            alt="Recycling"
+            alt="Person meditating" 
             className="ABrecycling-image"
           />
         </div>
         <div className="ABtext-section">
           <h2 className="ABtitle">
-            Why <span className="highlight">Recycle</span> Aluminum?
+            Why Recycle Aluminum?
           </h2>
           <p className="ABdescription">
             Aluminum recycling is crucial for environmental sustainability, economic growth,
             and energy conservation. Unlike many materials, aluminum can be recycled
             indefinitely without losing quality. Recycling aluminum saves up to
-            <strong> 95% of the energy </strong> required to produce new aluminum, significantly
+            95% of the energy required to produce new aluminum, significantly
             reducing greenhouse gas emissions and the carbon footprint. It also
             minimizes landfill waste, protects natural resources, and supports
             local jobs. Together, we can build a circular economy that benefits
